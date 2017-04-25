56ddfcde6860c812b8f6f32f828fe29b2310aac1
João Antôio <jacs@cin.ufpe.br>
2017-04-25 09:55:45 -0300

    Aula 25_04

--- 25_04.hs
+++ 25_04.hs
@@ -1,7 +1,7 @@
 type Ponto = (Float, Float, Float)
 distancia :: Ponto -> Ponto -> Float
 distancia (x1, y1, z1) (x2, y2, z2) =
-	sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+    sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
 
 type Nome = String
 type Tamanho = Int
@@ -12,3 +12,10 @@ size :: Diretorio -> (Tamanho, Int)
 size (Arquivo n t) = (t, 1)
 size (Pasta n ds) = foldl sumTuple (0,0) (map size ds)
                     where sumTuple = \(a,b) (c,d) -> (a+c,b+d)
+
+data Complexo = Zezinho Float Float
+instance Show Complexo where
+    show (Zezinho a b) = show a ++ signal ++ show b ++ ".j"
+        where signal = if(b>=0) then "+" else ""
+
+
